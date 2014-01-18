8b0ab1160e791c74ce5f24c1eab5c5045363385b
strburny <vadim.novikov@outlook.com>
2014-01-18 15:42:40 +0300

    added rating stars

--- js/route.js
+++ js/route.js
@@ -9,6 +9,7 @@ CinemasApp.CinemasRoute = Ember.Route.extend({
     // $.get( "http://afisha.tut.by/film.php?fid=3576").done(function( data ) {
     //    console.log("start");
     //    var textArea = $(data).find(".filmImage").find("img").attr('src');
+	//    var rating = $(data).find(".rating_s").attr('class');
     // });
     return this.store.find('cinema');
   }
