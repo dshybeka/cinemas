81c1fc8e7345553697fadf92352be4de0b4ca8b3
strburny <vadim.novikov@outlook.com>
2014-01-18 16:15:15 +0300

    added unic vk widget

--- js/app.js
+++ js/app.js
@@ -10,3 +10,7 @@ Ember.Handlebars.helper('format-markdown', function(input) {
 Ember.Handlebars.helper('format-markdown-minimized', function(input) {
     return new Handlebars.SafeString(showdown.makeHtml(input.substr(0,150) + "..."));
 });
+
+Ember.Handlebars.helper("vk-widget", function (page_id) {
+     VK.Widgets.Comments("vk_comments", {limit: 10, width: "496", attach: "*"}, page_id);   
+});
