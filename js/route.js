a149a13e4054ad4950971081f3c661278ba1c27f
Dzianis Shybeka <DzianisShybeka@eharmony.com>
2014-01-18 12:37:46 +0300

    Added images

--- js/route.js
+++ js/route.js
@@ -4,6 +4,10 @@ CinemasApp.Router.map(function () {
 
 CinemasApp.CinemasRoute = Ember.Route.extend({
   model: function () {
+    // $.get( "http://afisha.tut.by/film.php?fid=3576").done(function( data ) {
+    //    console.log("start");
+    //    var textArea = $(data).find(".filmImage").find("img").attr('src');
+    // });
     return this.store.find('cinema');
   }
 });
